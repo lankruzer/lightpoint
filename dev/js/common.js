00b06f97b11ea775a45137d75ab9d84c45f0a715
lan_kruzer <ar-48@yandex,ru>
2018-01-15 19:32:37 +0300

    complete project

--- dev/js/common.js
+++ dev/js/common.js
@@ -4,5 +4,35 @@ $(document).ready(function() {
 		autoplaySpeed: 7500,
 		dots: true,
 		loop: true
-	})	
+	})
+	
+	if(document.querySelector( '.btn_you-order' )) {
+		$( '.btn_you-order' ).click(function(event) {
+			event.preventDefault();
+			$( this ).toggleClass( 'btn_you-order_show');
+			$( '.aside-content' ).toggleClass( 'aside-content_show' );
+		})
+	}
+	
+	$( '.form ' ).submit(function(event) {
+		event.preventDefault();
+		call( $(this), $(this).attr('action') );
+	})
+	
+	function call(form, action) {
+		var msg = form.serialize();
+		$.ajax({
+			type: 'POST',
+			url: action,
+			data: msg,
+			success: function (data) {
+				console.log('Message succsess send, ' + data);
+			},
+			error: function (xhr, str) {
+				console.log('Возникла ошибка: ' + xhr.responseCode);
+			}
+		});
+
+	}
+	
 })
